31750ffb13bd12c74b3f8cc8dc7d4b15737cbbd2
theppitak13 <118673688+theppitak13@users.noreply.github.com>
2024-01-15 20:15:10 +0700

    Add files via upload

---
+++ w01/resources/views/bio.blade.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tailwind</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    
+
+<section class="pt-20 pb-10 lg:pt-[120px] lg:pb-20 bg-white dark:bg-dark">
+   <div class="container mx-auto">
+      <div class="flex flex-wrap justify-center -mx-4">
+         <div class="w-full px-4">
+            <div class="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
+               <span class="block mb-2 text-lg font-semibold text-primary">
+               W01
+               </span>
+               <p class="text-base text-body-color dark:text-dark-6">
+                เทพพิทักษ์ โภคทรัพย์ 6414421004
+               </p>
+            </div>
+         </div>
+      </div>
+      <div class="flex flex-wrap -mx-4">
+         <div class="w-full px-4 md:w-1/2 lg:w-1/3">
+            <div class="w-full mb-10">
+               <div class="mb-8 overflow-hidden rounded">
+                  <img
+                     src="https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-01.jpg"
+                     alt="image"
+                     class="w-full"
+                     />
+               </div>
+               <div>
+                  <span
+                     class="inline-block px-4 py-1 mb-5 text-xs font-semibold leading-loose text-center text-white rounded bg-primary"
+                     >
+                  Dec 22, 2023
+                  </span>
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        class="inline-block mb-4 text-xl font-semibold text-dark dark:text-white hover:text-primary sm:text-2xl lg:text-xl xl:text-2xl"
+                        >
+                     Meet AutoManage, the best AI management tools
+                     </a>
+                  </h3>
+                  <p class="text-base text-body-color dark:text-dark-6">
+                     Lorem Ipsum is simply dummy text of the printing and
+                     typesetting industry.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="w-full px-4 md:w-1/2 lg:w-1/3">
+            <div class="w-full mb-10">
+               <div class="mb-8 overflow-hidden rounded">
+                  <img
+                     src="https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-02.jpg"
+                     alt="image"
+                     class="w-full"
+                     />
+               </div>
+               <div>
+                  <span
+                     class="inline-block px-4 py-1 mb-5 text-xs font-semibold leading-loose text-center text-white rounded bg-primary"
+                     >
+                  Mar 15, 2023
+                  </span>
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        class="inline-block mb-4 text-xl font-semibold text-dark dark:text-white hover:text-primary sm:text-2xl lg:text-xl xl:text-2xl"
+                        >
+                     How to earn more money as a wellness coach
+                     </a>
+                  </h3>
+                  <p class="text-base text-body-color dark:text-dark-6">
+                     Lorem Ipsum is simply dummy text of the printing and
+                     typesetting industry.
+                  </p>
+               </div>
+            </div>
+         </div>
+         <div class="w-full px-4 md:w-1/2 lg:w-1/3">
+            <div class="w-full mb-10">
+               <div class="mb-8 overflow-hidden rounded">
+                  <img
+                     src="https://cdn.tailgrids.com/2.0/image/application/images/blogs/blog-01/image-03.jpg"
+                     alt="image"
+                     class="w-full"
+                     />
+               </div>
+               <div>
+                  <span
+                     class="inline-block px-4 py-1 mb-5 text-xs font-semibold leading-loose text-center text-white rounded bg-primary"
+                     >
+                  Jan 05, 2023
+                  </span>
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        class="inline-block mb-4 text-xl font-semibold text-dark dark:text-white hover:text-primary sm:text-2xl lg:text-xl xl:text-2xl"
+                        >
+                     The no-fuss guide to upselling and cross selling
+                     </a>
+                  </h3>
+                  <p class="text-base text-body-color dark:text-dark-6">
+                     Lorem Ipsum is simply dummy text of the printing and
+                     typesetting industry.
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+    
+</body>
+</html>
